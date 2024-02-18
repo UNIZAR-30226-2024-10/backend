4bf4f857f720d8f6bf162937314e7ed4a6ab3647
brizuelayeah <820773@unizar.es>
2024-02-18 14:03:20 +0100

    Actualización de las rutas principales

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 const userRouter = require("./routes/users")
 app.use("/users", userRouter)
 
+// Rutas para jugar
+const playRouter = require("./routes/play")
+app.use("/play", playRouter)
+
+// Rutas para la página principal
+const home_pageRouter = require("./routes/home_page")
+app.use("/users", home_pageRouter)
 
 
 
