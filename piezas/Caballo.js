d8ca0c7a10ef49d84cf58364f0f725da1895c49c
brizuelayeah <820773@unizar.es>
2024-03-05 18:50:20 +0100

    Finalizacion 1 de movimiento de piezas

--- piezas/Caballo.js
+++ piezas/Caballo.js
@@ -35,56 +35,112 @@ class Caballo {
         let casilla;
         if (this.isValidIndex(this.Posicion.x + 1, this.Posicion.y + 2, 8, 8)) {
             casilla = casillas[this.Posicion.x + 1][this.Posicion.y + 2];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
         
         if (this.isValidIndex(this.Posicion.x + 2, this.Posicion.y + 1, 8, 8)) {
             casilla = casillas[this.Posicion.x + 2][this.Posicion.y + 1];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
 
         if (this.isValidIndex(this.Posicion.x - 1, this.Posicion.y + 2, 8, 8)) {
             casilla = casillas[this.Posicion.x - 1][this.Posicion.y + 2];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y + 2, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
 
         if (this.isValidIndex(this.Posicion.x - 2, this.Posicion.y + 1, 8, 8)) {
             casilla = casillas[this.Posicion.x - 2][this.Posicion.y + 1];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y + 1, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
 
         if (this.isValidIndex(this.Posicion.x - 1, this.Posicion.y - 2, 8, 8)) {
             casilla = casillas[this.Posicion.x - 1][this.Posicion.y - 2];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x - 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
 
         if (this.isValidIndex(this.Posicion.x - 2, this.Posicion.y - 1, 8, 8)) {
             casilla = casillas[this.Posicion.x - 2][this.Posicion.y - 1];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x - 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
 
         if (this.isValidIndex(this.Posicion.x + 1, this.Posicion.y - 2, 8, 8)) {
             casilla = casillas[this.Posicion.x + 1][this.Posicion.y - 2];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x + 1, this.Posicion.y - 2, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
         if (this.isValidIndex(this.Posicion.x + 2, this.Posicion.y - 1, 8, 8)) {
             casilla = casillas[this.Posicion.x + 2][this.Posicion.y - 1];
-            if (casilla !== undefined && casilla !== null && casilla.getPieza() === null) {
-                this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+            if (casilla !== undefined && casilla !== null) {
+                if(casilla.getPieza() === null) {
+                    this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+                }
+                else {
+                    if(casilla.getPieza().getColor() !== this.color) {
+                        this._agregarMovimiento(this.Posicion.x + 2, this.Posicion.y - 1, movimientos_disponibles_caballo);
+                    }
+                }
             }
         }
         return movimientos_disponibles_caballo;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 // Configurar middleware para analizar el cuerpo de las solicitudes como JSON
-// app.use(express.json());
 app.use(bodyParser.json());
 // Rutas de los usuarios
 const userRouter = require("./routes/users")
